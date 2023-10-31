83fc87a1ea36c6c12a82b8c362474f6be814a085
Asaad Sah <asaadsahli@gmail.com>
2023-10-31 13:25:11 +0100

    commit to my gitHub

---
+++ src/Controller/ReservationsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ReservationRepository;
+use Nelmio\CorsBundle\Annotation\Cors;
+
+use Symfony\Bundle\WebProfilerBundle\Csp\ContentSecurityPolicyHandler;
+use Symfony\Component\HttpFoundation\Request;
+
+class ReservationsController extends AbstractController
+{
+    #[Route('/reservations', name: 'app_reservations') ]
+    
+    public function index(Request $request,ReservationRepository $reservationRepository): JsonResponse
+    {
+
+
+
+        $reservations = $reservationRepository->findAllReservations();
+        //dd($reservations);
+        $data = [];
+        foreach ($reservations as $reservation) {
+            $data[] = [
+                'id' => $reservation->getId(),
+                'dateReservation' => $reservation->getDateReservation()->format('Y-m-d'),
+                'dateEntree' => $reservation->getDateEntree()->format('Y-m-d'),
+                'dateSortie' => $reservation->getDateSortie()->format('Y-m-d'),
+                'user' => [
+                    'id' => $reservation->getUser()->getId(),
+                    
+                ],
+                'chambre' => [
+                    'id' => $reservation->getChambre()->getId(),
+                    
+                ],
+            ];
+        }
+
+        return $this->json([
+            'messages'  =>$data]);
+    }
+}
---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+use Symfony\Component\Validator\Constraints\Regex;
+
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+ 
+            ->add('plainPassword', RepeatedType::class, [
+ 
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+
+                //             'message' => 'Please enter a password',
+                //         ]),
+                //         new Length([
+                //             'min' => 6,
+                //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+                //              max length allowed by Symfony for security reasons
+                //             'max' => 4096,
+                //         ]),
+                //     ],
+                //     'label' => 'New password',
+                // ],
+                // 'second_options' => [
+                //     'label' => 'Repeat Password',
+                // ],
+                // 'invalid_message' => 'The password fields must match.',
+
+                            'message' => 'Entrez un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new Regex([
+                            'pattern' => '/^(?=.*[A-Z])(?=.*\d)/',
+                            'message' => 'Votre mot de passe doit contenir au moins une lettre majuscule et au moins un chiffre.',
+                        ]),
+                    ],
+                    'label' => 'Nouveau mot de passe',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre nouveau mot de passe',
+                ],
+                'invalid_message' => 'Mot de passe non identique',
+
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
